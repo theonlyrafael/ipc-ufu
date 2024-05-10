d2ae149b0223e49c69e78b258810b6cbb9986583
Rafael Fraga <rafalfraga@gmail.com>
2024-05-10 10:00:00 -0300

    Subindo atividade de string

---
+++ AtividadesLab/String/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+
+
+// Aula11b-LaboratorioStrings.pdf
+// Exercicio 3
+
+
+
+int main() {
+    char str[200]; printf("Texto: "); gets(str);
+
+
+
+    int tamanho = strlen(str);
+    for (int i = 0; i < tamanho / 2; i++) {
+        int aux = str[i];
+        str[i] = str[tamanho - 1 - i];
+        str[tamanho - 1 - i] = aux;
+    }
+
+
+
+    printf("Texto: %s", str);
+    return 0;
+}
