e80f448ad16fc73fbe46f90e4a82f9ab395c807c
Rafael Fraga <rafalfraga@gmail.com>
2024-08-07 21:45:00 -0300

    Adicionando trabalho 16 de alocacao dinamica de matrizes e transposicao

---
+++ Trabalhos/trab16.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void matrizes(){
+    float **matA;
+    int i,j;
+
+    matA = malloc(5 * sizeof(float*));
+
+    for(i=0;i<5;i++){
+        matA[i] = malloc (5 * sizeof (float));
+    }
+
+    for (i=0; i < 5; i++){
+        for (j=0; j < 5; j++){
+            printf("matA[%d][%d]: ", i,j);
+            scanf("%f", &matA[i][j]);
+        }
+    }
+
+    printf("Matriz A:\n");
+    for(i=0; i<5; i++) {
+        for(j=0;j<5; j++) {
+            printf("%.1f ", matA[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\n\n");
+    printf("Matriz B(Transposta):\n");
+    for(i=0; i<5; i++) {
+        for(j=0;j<5; j++) {
+            printf("%.1f ",matA[j][i]);
+        }
+        printf("\n");
+    }
+
+    for(i=0; i<5; i++){
+        free(matA[i]);
+    }
+    free(matA);
+}
+
+int main(){
+    matrizes();
+    return 0;
+}
